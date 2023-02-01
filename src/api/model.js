b04cff444f6fcf19f884383ad7f1cbef7e2057fe
SandroIerna <iernasandro@gmail.com>
2023-02-01 16:53:49 +0100

    Adds CRUD for Product

---
+++ src/api/model.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import sequilize from "../db.js";
+
+const ProductModel = sequilize.define("product", {
+  id: {
+    type: DataTypes.UUID,
+    primaryKey: true,
+    defaultValue: DataTypes.UUIDV4,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+export default ProductModel;
